790680fbedf3c1de7044fdd0e68821535d0f334d
Jessica Hille <jessicalhille@gmail.com>
2021-12-31 14:26:20 -0600

    completed creation of index.html file, need to finalize css

--- index.js
+++ index.js
@@ -1,2 +1,220 @@
+// required packages
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+// required files in lib folder
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// the HTML creation based on user input
+const createHTML = require('./src/htmlTemplate');
+
+// create the array for the team
+const officeArray = [];
+
+// manager questions
+const init = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter the name of the manager.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('A manager name is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter the id number for this manager.',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('An id number is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter the email address for this manager.',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('An email address is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Please enter the office number for this manager.',
+            validate: officeNumberInput => {
+                if (officeNumberInput) {
+                    return true;
+                } else {
+                    console.log('An office number is required.');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+
+        officeArray.push(manager);
+        console.log(manager);
+    })
+};
+
+const addEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Please select which role this employee has.',
+            choices: ['Engineer', 'Intern'],
+            validate: roleInput => {
+                if (roleInput) {
+                    return true;
+                } else {
+                    console.log('A role selection is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter the name of this employee.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('An employee name is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter the id number for this employee.',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('An id number is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter the email address for this employee.',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('An email address is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter the github username for this engineer.',
+            // this question will only appear if the user selects Engineer in the role question
+            when: (input) => input.role === 'Engineer',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('A github username is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Please enter the name of the school that this intern attended.',
+            // this question will only appear if the user selects Intern in the role question
+            when: (input) => input.role === 'Intern',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log('A school name is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add another employee?',
+            default: 'false'
+        }
+    ])
+    .then(employeeInput => {
+        let { name, id, email, role, github, school, confirmAddEmployee } = employeeInput;
+        let employee;
+
+        if (role === 'Engineer') {
+            employee = new Engineer (name, id, email, github);
+
+            console.log(employee);
+        } else {
+            employee = new Intern (name, id, email, school);
+
+            console.log(employee);
+        }
+
+        officeArray.push(employee);
+
+        if (confirmAddEmployee) {
+            return addEmployee(officeArray);
+        } else {
+            return officeArray;
+        }
+    })
+}
+
+// function to generate an HTML file from user input
+const writeToFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there are any errors
+        if (err) {
+            throw err;
+        } else {
+            console.log('Your office team profile was successfully created!');
+        }
+    })
+}
+
+// function call to initialize app
+init()
+    .then(addEmployee)
+    .then(officeArray => {
+        return createHTML(officeArray);
+    })
+    .then(renderHTML => {
+        return writeToFile(renderHTML);
+    })
+    .catch(err => {
+        console.log(err);
+    });
---
+++ src/htmlTemplate.js
@@ -0,0 +1,139 @@
+// create the manager card
+const createManager = function(manager) {
+    return `
+    <div class="col-4">
+        <div class="card">
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+                <h3><i class="fas fa-mug-hot"></i>Manager</h3>
+            </div>
+
+            <div class="card-body">
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="officeNumber">Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+// create the engineer card
+const createEngineer = function(engineer) {
+    return `
+    <div class="col-4">
+        <div class="card">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h3><i class="fas fa-glasses"></i>Engineer</h3>
+            </div>
+
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+// create the intern card
+const createIntern = function(intern) {
+    return `
+    <div class="col-4">
+        <div class="card">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h3><i class="fas fa-user-graduate"></i>Intern</h3>
+            </div>
+
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+// pulls all the data into one function so that multiple cards of the same role can be created
+createHTML = (data) => {
+    htmlArray = [];
+
+    // tracks how many cards to create with the data
+    for (let i=0; i < data.length; i++) {
+        const employee = data[i];
+        // defines which role to use
+        const role = employee.getRole();
+
+        if (role === 'Manager') {
+            // create manager card with data and html code above
+            const managerCard = createManager(employee);
+            // push the manager card data into the array
+            htmlArray.push(managerCard);
+        }
+
+        if (role === 'Engineer') {
+            // create engineer card with data and html code above
+            const engineerCard = createEngineer(employee);
+            // push the engineer card data into the array
+            htmlArray.push(engineerCard);
+        }
+
+        if (role === 'Intern') {
+            // create intern card with data and html code above
+            const internCard = createIntern(employee);
+            // push the intern card data into the array
+            htmlArray.push(internCard)
+        }
+    }
+
+    // joining all the strings together to create the cards
+    const allTeamCards = htmlArray.join('');
+
+    // takes the cards and pushes them to the function to create the HTML page
+    const generateTeam = createHTMLPage(allTeamCards);
+    return generateTeam;
+}
+
+// function to create and generate the HTML page using the cards created above
+const createHTMLPage = function(allTeamCards) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Team Profile Page</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="style.css" />
+    </head>
+
+    <body>
+        <header>
+            <section class="navbar">
+                <h1 class="header-text">My Team</h1>
+            </section>
+        </header>
+
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="cards">
+                    ${allTeamCards}
+                </div>
+            </div>
+        </main>
+    </body>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    `;
+}
+
+// export to the index file
+module.exports = createHTML;
